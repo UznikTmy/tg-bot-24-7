5f464252074e0926f6898c8d4e8387fae736e1dd
UznikTmy <lm8897715@gmail.com>
2026-01-11 02:03:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ bot = telebot.TeleBot("8287514660:AAGmIWrcoydKFDWl65tb_zRrkpsFKiMEFjw")
 
 @bot.message_handler(func=lambda _: True)
 def reply(message):
-  bot.reply_to(message, "I'm alive 24/7")
+  bot.reply_to(message, "Hello! I'm alive 24/7.")
+
+bot.
 
 bot.polling(none_stop=True)
