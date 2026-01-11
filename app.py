3610927869d87062254c5e7c0d4b331b4bda69e1
UznikTmy <lm8897715@gmail.com>
2026-01-11 11:36:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,21 +4,16 @@ import schedule
 
 bot = telebot.TeleBot("8287514660:AAGmIWrcoydKFDWl65tb_zRrkpsFKiMEFjw")
 
-msg = "";
-
 @bot.message_handler(func=lambda _: True)
 def reply(message):
-  msg = message;
   now = datetime.datetime.now()
   date_time = now.strftime("%Y-%m-%d %H:%M:%S")
   bot.reply_to(message, "Привет! Я доступен. Сейчас "+date_time)
 
-def job(message):
+def job():
   now = datetime.datetime.now()
   date_time = now.strftime("%Y-%m-%d %H:%M:%S")
-  bot.reply_to(message, "Привет! Я работаю... ("+date_time+")")
 
-# Schedule the job to run every 10 seconds
-schedule.every(10).seconds.do(job, message=msg)
+schedule.every(10).seconds.do(job)
 
 bot.polling(none_stop=True)
