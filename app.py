f7bdbb0dbdafe8d52a3a9b2c32b2460ce5244ed9
UznikTmy <lm8897715@gmail.com>
2026-01-11 12:02:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ bot = telebot.TeleBot("8287514660:AAGmIWrcoydKFDWl65tb_zRrkpsFKiMEFjw")
 def reply(message):
   now = datetime.datetime.now()
   date_time = now.strftime("%Y-%m-%d %H:%M:%S")
-  bot.reply_to(message, "Привет! Я доступен. Сейчас "+date_time)
+  bot.reply_to(message, "Привет! Я доступен. \n Сейчас "+date_time)
 
 def delayed():
   th_name = threading.current_thread().name
   print(f'Th:{th_name} Worker запущен')
-  bot.send_message(1949621819, "Привет! Я доступен по таймеру.")
+  bot.send_message(-1003613605265, "Привет! Я доступен по таймеру.") # 1949621819
 
 # Создание и запуск потоков таймеров
 t1 = threading.Timer(10, delayed)
