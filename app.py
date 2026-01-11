e14e3c92bc40b696e2c9d12c16c7cabda6c68a03
UznikTmy <lm8897715@gmail.com>
2026-01-11 10:30:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import telebot
+from datetime import date
 
 bot = telebot.TeleBot("8287514660:AAGmIWrcoydKFDWl65tb_zRrkpsFKiMEFjw")
 
 @bot.message_handler(func=lambda _: True)
 def reply(message):
-  bot.reply_to(message, "Hello! I'm alive 24/7.")
+  today = date.today()
+  formatted_date = today.strftime("%Y-%m-%d")
+  bot.reply_to(message, "Привет! Я доступен. Сейчас "+formatted_date)
 
 bot.polling(none_stop=True)
